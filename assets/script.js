fefd09af5dd548dd72d4c65a7f3a7becb8381d6e
SirPotatoIV <otoolej33@gmail.com>
2020-04-22 12:18:02 -0500

    Sound is now playing when the buttons are clicked.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
 function main() {
+  const synth = new Tone.Synth().toDestination();
+
+  //   function bringTheNoise() {
+  //     const synth = new Tone.Synth().toDestination();
+  //     synth.triggerAttackRelease("C4", "8n");
+  //   }
+
   function buttonEvents() {
     const button1 = document.getElementById("button1");
     const button2 = document.getElementById("button2");
@@ -6,12 +13,15 @@ function main() {
 
     button1.addEventListener("click", function () {
       console.log("button 1");
+      synth.triggerAttackRelease("C4", "8n");
     });
     button2.addEventListener("click", function () {
       console.log("button 2");
+      synth.triggerAttackRelease("C5", "8n");
     });
     button3.addEventListener("click", function () {
       console.log("button 3");
+      synth.triggerAttackRelease("C6", "8n");
     });
   }
   buttonEvents();
