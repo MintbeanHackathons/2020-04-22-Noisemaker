83237879fb5d295213a4f13391df2e2aabbfb6ba
SirPotatoIV <otoolej33@gmail.com>
2020-04-22 12:01:24 -0500

    Added an event listener for each button and confirmed they were working with a console log that occurs on click. Added minor styling to center text. Changed text in html to have more accurate title.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 function main() {
-  console.log("test");
-  console.log(Tone);
+  function buttonEvents() {
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const button3 = document.getElementById("button3");
+
+    button1.addEventListener("click", function () {
+      console.log("button 1");
+    });
+    button2.addEventListener("click", function () {
+      console.log("button 2");
+    });
+    button3.addEventListener("click", function () {
+      console.log("button 3");
+    });
+  }
+  buttonEvents();
 }
 main();
