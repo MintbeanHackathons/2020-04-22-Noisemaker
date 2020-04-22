0b1880b9a8f25db97c64ad0092b41017c9d11272
Ismael <ismael.melendez@hotmail.com>
2020-04-22 12:29:07 -0500

    Made boxes using materialUI
    Added boxes to page using material UI. These will act as buttons to be pressed.

--- player/src/App.js
+++ player/src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { Typography, Container } from '@material-ui/core';
+import { Typography, Container, makeStyles, Paper, Grid } from '@material-ui/core';
 
-
+const useStyles = makeStyles((theme)=>({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <Container maxWidth="sm">
+      <Container maxWidth={classes.root}>
       {/* <h1>@IsmaelTechNow Noise Toy </h1> */}
-      <Typography variant="h1" component="h2" gutterBottom>
+      <Typography variant="h1" gutterBottom>
       @IsmaelTechNow Noise Toy
       </Typography>
+      <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+      </div>
       </Container>
     </div>
   );
