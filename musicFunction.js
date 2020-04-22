de6b6bafeeff13b63f2a33cc59d058d8af1b90d7
nsibo429 <nsibo@uwo.ca>
2020-04-22 14:18:30 -0400

    different sounds for different flowers

--- musicFunction.js
+++ musicFunction.js
@@ -1,19 +1,33 @@
 var original;
 var i = 0;
+
 window.onload = function() {
 original = document.getElementById('container');
 
 };
 
-function playSound(){
-	const synth = new Tone.Synth().toMaster();
+function playSound(id){
+	//duplicate 
+	var clone = original.cloneNode(true); // clone
+	clone.id = "container" + ++i; // there can only be one element with an ID
+	original.parentNode.appendChild(clone);
+	
+	sound(id);
+	
+}
 
-	//play a middle 'C' for the duration of an 8th note
-	synth.triggerAttackRelease("C4", "8n");
-	if(original){
-		
-		var clone = original.cloneNode(true); // "deep" clone
-		clone.id = "container" + ++i; // there can only be one element with an ID
-		original.parentNode.appendChild(clone);
+function sound(x){
+	const synth = new Tone.Synth().toMaster();
+	
+	if(x == "container"){
+		synth.triggerAttackRelease("C4", "8n");
 	}
+	
+	if(x == "container1"){
+		synth.triggerAttackRelease("C5", "8n");
+	}
+	if(x == "container2"){
+		synth.triggerAttackRelease("C5", "8n");
+	}
+	
 }
