87abb6a36b7df2e0ea773fb86c3f7d2c1401a1b1
nsibo429 <nsibo@uwo.ca>
2020-04-22 14:45:45 -0400

    10 sounds

--- musicFunction.js
+++ musicFunction.js
@@ -11,7 +11,7 @@ function playSound(id){
 	var clone = original.cloneNode(true); // clone
 	clone.id = "container" + ++i; // there can only be one element with an ID
 	original.parentNode.appendChild(clone);
-	
+	//play sound
 	sound(id);
 	
 }
@@ -20,12 +20,30 @@ function sound(x){
 	const synth = new Tone.Synth().toMaster();
 	var num = x.slice(9);
 	num = parseInt(num);
-
+	//play different notes randomly based on number of flower
 	if(num % 2 == 0){
 		synth.triggerAttackRelease("C3", "8n");
 	}
 	else if(num % 3 == 0){
 		synth.triggerAttackRelease("C5", "8n");
+	}
+	else if(num % 4 == 0){
+		synth.triggerAttackRelease("B3", "8n");
+	}
+	else if(num % 5 == 0){
+		synth.triggerAttackRelease("D3", "8n");
+	}
+	else if(num % 6 == 0){
+		synth.triggerAttackRelease("B5", "8n");
+	}
+	else if(num % 7 == 0){
+		synth.triggerAttackRelease("D5", "8n");
+	}
+	else if(num % 8 == 0){
+		synth.triggerAttackRelease("E5", "8n");
+	}
+	else if(num % 9 == 0){
+		synth.triggerAttackRelease("E3", "8n");
 	}
 	else{
 		synth.triggerAttackRelease("C4", "8n");
