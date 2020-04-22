e759bb9a76d0e5ec1c41621b47bd2a5f09dd9606
yulia-kh <y.khisamutdinova@gmail.com>
2020-04-22 11:40:57 -0700

    Add style

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-
-
 document.querySelector('button').addEventListener('click', async () => {
-	await Tone.start()
-    console.log('audio is ready')
-    //create a synth and connect it to the master output (your speakers)
-    var synth = new Tone.Synth().toMaster()
+  const button = document.getElementById('boom');
+  button.style.height = '150px';
+  button.style.width = '200px';
 
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4', '8n')
+	await Tone.start()
+  //create a synth and connect it to the master output (your speakers)
+  var synth = new Tone.Synth().toMaster()
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease('C4', '8n')
 })
 
+document.querySelector('button').addEventListener('mouseout', async () => {
+  const button = document.getElementById('boom');
+  button.style.height = '100px';
+  button.style.width = '150px';
+});
