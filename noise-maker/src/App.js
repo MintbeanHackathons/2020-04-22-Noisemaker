585b095eef29aacb7f2ef5853e10943863d4a990
Carlos Del Real <carloshdelreal@gmail.com>
2020-04-22 13:44:02 -0500

    center title

--- noise-maker/src/App.js
+++ noise-maker/src/App.js
@@ -5,19 +5,19 @@ import SynteL from './containers/syntetizerLow';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
+      <header className="App-header p-4">
+        <h1 className="text-center">
           Carlos's Syte
         </h1>
       </header>
       <main>
         <div className="container">
-          <div className="row justify-content-center">
+          <div className="row justify-content-center m-3">
             <div className="col-8 justify-content-center">
               <SynteL />
             </div>
           </div>
-          <div className="row justify-content-center">
+          <div className="row justify-content-center m-3">
             <div className="col-8 justify-content-center">
               <SynteH />
             </div>
