a92e94d2b9b23e8050ab338bfed234b1457fec54
Antanas Tricou <APTricou@gmail.com>
2020-04-22 11:58:50 -0500

    Boilerplate

---
+++ src/Styles/FlexContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const FlexContainer = styled.div/*css*/ `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 1rem;
+`
+export default FlexContainer
---
+++ src/store/reducers/reducerA.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const SET_VALUE = 'SET_VALUE'
+
+/**
+ * INITIAL STATE
+ */
+const initialState = {
+  value: 0,
+}
+
+/**
+ * ACTION CREATORS
+ */
+export const setValue = (value) => ({
+  type: SET_VALUE,
+  value,
+})
+
+/**
+ * THUNK CREATORS
+ */
+// export const fetchRandomCard = () => async (dispatch) => {
+//   const {data} = await axios.get('/api/cards/random')
+//   dispatch(setCard(data))
+// }
+
+/**
+ * REDUCER
+ */
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_VALUE:
+      return {...state, value: action.value}
+    default:
+      return state
+  }
+}
