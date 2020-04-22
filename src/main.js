934674b06db8f68461592e819a13139cca264daa
Derek Johnston <Derek@dj-design.ca>
2020-04-22 14:37:05 -0400

    Basic tone generator with some options

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue } from 'bootstrap-vue'
-
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
