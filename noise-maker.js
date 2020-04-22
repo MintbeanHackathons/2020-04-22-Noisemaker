37a04d81d82c9b40859d1a9a7ae441d31f41094e
Paul R. Carlson <carlsonpar@gmail.com>
2020-04-22 14:49:00 -0400

    changed some of the javascript functions, added new abilities

--- noise-maker.js
+++ noise-maker.js
@@ -1,58 +1,44 @@
-const synth= new Tone.Synth();
-synth.toMaster();
-//synth.triggerAttackRelease("C4","8n");
+const Synth= new Tone.Synth();
+Synth.toMaster();
+//Synth.triggerAttackRelease("C4","8n");
 
-const green = document.getElementById("green");
+const Green = document.getElementById("green");
 green.addEventListener("mousedown",e=>{
   const note = e.target.dataset.note
-  synth.triggerAttackRelease(note,"8n")
+  Synth.triggerAttackRelease(note,"8n")
 });
-const blue = document.getElementById("blue");
+const Blue = document.getElementById("blue");
 blue.addEventListener("mousedown",e=>{
   const note = e.target.dataset.note
-  synth.triggerAttackRelease(note,"8n")
+  Synth.triggerAttackRelease(note,"8n")
 });
-const red = document.getElementById("red");
+const Red = document.getElementById("red");
 red.addEventListener("mousedown",e=>{
   const note = e.target.dataset.note
-  synth.triggerAttackRelease(note,"8n")
+  Synth.triggerAttackRelease(note,"8n")
 });
-const yellow = document.getElementById("yellow");
+const Yellow = document.getElementById("yellow");
 yellow.addEventListener("mousedown",e=>{
   const note = e.target.dataset.note
-  synth.triggerAttackRelease(note,"8n")
+  Synth.triggerAttackRelease(note,"8n")
 });
 
+//allows you to play the notes using your keyboard.  
 document.addEventListener("keydown",e=>{
   switch(e.key){
     case "q":
-      synth.triggerAttackRelease(green.dataset.note,"8n");
+      Synth.triggerAttackRelease(green.dataset.note,"8n");
       return;
     case "w":
-      synth.triggerAttackRelease(blue.dataset.note,"8n");
+      Synth.triggerAttackRelease(blue.dataset.note,"8n");
       return;
     case "a":
-      synth.triggerAttackRelease(red.dataset.note,"8n");
+      Synth.triggerAttackRelease(red.dataset.note,"8n");
       return;
     case "s":
-      synth.triggerAttackRelease(yellow.dataset.note,"8n");
+      Synth.triggerAttackRelease(yellow.dataset.note,"8n");
       return;
     default:
       return;
   }
 })
-const RecordButton = document.getElementById("record");
-const PlayButton = document.getElementById("play");
-let Recording = false;
-let song=[];
-RecordButton.onclick=()=>{
-  console.log(Recording);
-  Recording = !Recording;
-  return Recording
-}
-console.log("new line " + Recording);
-const gamespace = document.getElementById("board");
-gamespace.onclick = (e) => {
-  song.push(e.target.dataset.note);
-};
-console.log(song)
