8c66d798852c6e9be1e8910b4d659ca3c3d50761
amandena <manda.panda91@yahoo.com>
2020-04-22 13:17:41 -0400

    added comments

--- boredInTheHouse.js
+++ boredInTheHouse.js
@@ -1,14 +1,20 @@
+//creates instance of audio context
 const AudioContext = window.AudioContext || window.webkitAudioContext
 const audioContext = new AudioContext()
 
+//gets audio element
 const audioElement = document.querySelector('audio')
+
+// passes audio element into the audio context
 const track = audioContext.createMediaElementSource(audioElement)
 
+// connects element to contect destination
 track.connect(audioContext.destination)
 
+// creates play button
 const playButton = document.querySelector('button')
 
-playButton.addEventListener('click', function() {
+playButton.addEventListener('click', function() { // allows play and pause
   if(audioContext.state === 'suspended') {
     audioContext.resume()
   }
@@ -20,3 +26,8 @@ playButton.addEventListener('click', function() {
     this.dataset.playing === 'false'
   }
 }, false)
+
+
+
+
+
