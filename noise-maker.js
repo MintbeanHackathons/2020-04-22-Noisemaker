12e8ef6ddb8263dd49770b3b2ae7e1b4356e8946
Paul R. Carlson <carlsonpar@gmail.com>
2020-04-23 22:06:03 -0400

    updated the record play buttons to work.  Also connected the keys to the record as well

--- noise-maker.js
+++ noise-maker.js
@@ -1,45 +1,82 @@
-// import Tone from './node_modules/tone';
-console.log("good start");
-const Synth= new Tone.Synth();
+const Synth = new Tone.Synth();
 Synth.toMaster();
-Synth.triggerAttackRelease("C4","8n");
+Synth.triggerAttackRelease("C4", "8n");
 
-const Green = document.getElementById('green');
-Green.addEventListener("mousedown",e=>{
-  const note = e.target.dataset.note
-  Synth.triggerAttackRelease(note,"8n")
+recording = false;
+song = [];
+
+const Green = document.getElementById("green");
+Green.addEventListener("mousedown", (e) => {
+  const note = e.target.dataset.note;
+  Synth.triggerAttackRelease(note, "8n");
 });
 const Blue = document.getElementById("blue");
-Blue.addEventListener("mousedown",e=>{
-  const note = e.target.dataset.note
-  Synth.triggerAttackRelease(note,"8n")
+Blue.addEventListener("mousedown", (e) => {
+  const note = e.target.dataset.note;
+  Synth.triggerAttackRelease(note, "8n");
 });
 const Red = document.getElementById("red");
-Red.addEventListener("mousedown",e=>{
-  const note = e.target.dataset.note
-  Synth.triggerAttackRelease(note,"8n")
+Red.addEventListener("mousedown", (e) => {
+  const note = e.target.dataset.note;
+  Synth.triggerAttackRelease(note, "8n");
 });
 const Yellow = document.getElementById("yellow");
-Yellow.addEventListener("mousedown",e=>{ const note = e.target.dataset.note
-  Synth.triggerAttackRelease(note,"8n")
+Yellow.addEventListener("mousedown", (e) => {
+  const note = e.target.dataset.note;
+  Synth.triggerAttackRelease(note, "8n");
 });
+const Board = document.getElementById("board");
+Board.onclick = (e) => {
+  if (recording) {
+    song.push(e.target.dataset.note);
+  }
+};
 
-//allows you to play the notes using your keyboard.  
-document.addEventListener("keydown",e=>{
-  switch(e.key){
+const RecordButton = document.getElementById("record");
+RecordButton.onclick = () => {
+  //console.log(recording)
+  recording = !recording;
+  if (!recording) {
+    console.log(song);
+  }
+};
+const PlayRecordingButton = document.getElementById("play");
+
+PlayRecordingButton.onclick = () => {
+  //const song = ["C3", "Eb3", "G3", "Bb3"];
+  const SongPart = new Tone.Sequence(
+    function (time, note) {
+      Synth.triggerAttackRelease(note,"10hz", time);
+      console.log(note)
+    },
+    song,
+    "8n"
+  );
+  SongPart.start();
+  
+  Tone.Transport.start();
+};
+
+//allows you to play the notes using your keyboard.
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
     case "q":
-      Synth.triggerAttackRelease(green.dataset.note,"8n");
+      Synth.triggerAttackRelease(Green.dataset.note, "8n");
+      if (recording) {song.push(Green.dataset.note);}
       return;
     case "w":
-      Synth.triggerAttackRelease(blue.dataset.note,"8n");
+      Synth.triggerAttackRelease(Blue.dataset.note, "8n");
+      if (recording) {song.push(Blue.dataset.note);}
       return;
     case "a":
-      Synth.triggerAttackRelease(red.dataset.note,"8n");
+      Synth.triggerAttackRelease(Red.dataset.note, "8n");
+      if (recording) {song.push(Red.dataset.note);}
       return;
     case "s":
-      Synth.triggerAttackRelease(yellow.dataset.note,"8n");
+      Synth.triggerAttackRelease(Yellow.dataset.note, "8n");
+      if (recording) {song.push(Yellow.dataset.note);}
       return;
     default:
       return;
   }
-})
+});
