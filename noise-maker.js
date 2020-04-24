611b1051f946f170dda0516eafcf5b4b364b9bbd
Paul R. Carlson <carlsonpar@gmail.com>
2020-04-24 10:37:52 -0400

    updated the play record buttons to reflect the current state.  Fixed repeat error with the playback

--- noise-maker.js
+++ noise-maker.js
@@ -35,27 +35,45 @@ Board.onclick = (e) => {
 
 const RecordButton = document.getElementById("record");
 RecordButton.onclick = () => {
-  //console.log(recording)
+  RecordButton.innerHTML="Recording ....";
   recording = !recording;
   if (!recording) {
-    console.log(song);
+    RecordButton.innerHTML="Record"
   }
 };
 const PlayRecordingButton = document.getElementById("play");
 
 PlayRecordingButton.onclick = () => {
+  console.log(playing);
+  playing = !playing;
+
   //const song = ["C3", "Eb3", "G3", "Bb3"];
   const SongPart = new Tone.Sequence(
     function (time, note) {
-      Synth.triggerAttackRelease(note,"10hz", time);
-      console.log(note)
+      Synth.triggerAttackRelease(note, "10hz");
+      // console.log(note);
     },
     song,
     "8n"
   );
   SongPart.start();
-  
+
   Tone.Transport.start();
+  if (playing) {
+    console.log('play song');
+    SongPart.start();
+
+    Tone.Transport.start();
+    document.getElementById('play').innerHTML='stop';
+  }
+  else{
+    console.log('stop song');
+    SongPart.stop();
+    Tone.Transport.stop();
+    Tone.Transport.position = 0;
+    Tone.Transport.cancel();
+    document.getElementById('play').innerHTML='play';
+  }
 };
 
 //allows you to play the notes using your keyboard.
@@ -63,19 +81,27 @@ document.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "q":
       Synth.triggerAttackRelease(Green.dataset.note, "8n");
-      if (recording) {song.push(Green.dataset.note);}
+      if (recording) {
+        song.push(Green.dataset.note);
+      }
       return;
     case "w":
       Synth.triggerAttackRelease(Blue.dataset.note, "8n");
-      if (recording) {song.push(Blue.dataset.note);}
+      if (recording) {
+        song.push(Blue.dataset.note);
+      }
       return;
     case "a":
       Synth.triggerAttackRelease(Red.dataset.note, "8n");
-      if (recording) {song.push(Red.dataset.note);}
+      if (recording) {
+        song.push(Red.dataset.note);
+      }
       return;
     case "s":
       Synth.triggerAttackRelease(Yellow.dataset.note, "8n");
-      if (recording) {song.push(Yellow.dataset.note);}
+      if (recording) {
+        song.push(Yellow.dataset.note);
+      }
       return;
     default:
       return;
